2a54164352787cf2cfe2f72c9f05058588bc36fc
Salma-Essam-abdullah <salma.essam3212@gmail.com>
2024-06-30 22:54:06 +0300

    last refactor

--- Ultatel.BusinessLoginLayer/obj/Debug/net8.0/Ultatel.BusinessLoginLayer.AssemblyInfo.cs
+++ Ultatel.BusinessLoginLayer/obj/Debug/net8.0/Ultatel.BusinessLoginLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ultatel.BusinessLoginLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af305935f5952f977d7e932c6973258bb77221e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c07215281d3c588ae28ccedfb7a759fdf861cc2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ultatel.BusinessLoginLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ultatel.BusinessLoginLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
